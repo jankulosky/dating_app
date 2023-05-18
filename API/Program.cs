72da3b27d14da94dddddc800a4f8019d30a48339
Viktor Jankuloski <viktor.jankuloski@hotmail.com>
2023-05-18 21:10:56 +0200

    pre switch to postgres

--- API/Program.cs
+++ API/Program.cs
@@ -52,11 +52,15 @@ app.UseCors(MyAllowSpecificOrigins);
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllers();
     endpoints.MapHub<PresenceHub>("hubs/presence");
     endpoints.MapHub<MessageHub>("hubs/message");
+    endpoints.MapFallbackToController("Index", "Fallback");
 });
 
 using var scope = app.Services.CreateScope();
